7f0d070f492143822be7e86ef734bc2a0d6d3a2c
Andrew Cartwright <andrewlloydcartwright@gmail.com>
2016-09-11 13:26:11 -0500

    Fixed params and syntax errors

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+import time
 import praw
 pr = praw.Reddit(user_agent='Birmingham apartment hunter notifier by /u/NotFlameRetardant')
-pr.login = ('username', 'password') # Replace these with creds of account to send from
+pr.login()
 keywords = ['apartment', 'apartments', 'hunting', 'looking for apartment', 'searching', 'rent', 'lease']
-sub = pr.get_subreddit('birmingham')
+sub = pr.get_subreddit('test')
 already_done = []
 
 while True:
-    for submission in sub.get_hot(limit=10)
+    for submission in sub.get_hot(limit=10):
         title = submission.title.lower()
-        body = submission.text.lower()
+        body = submission.selftext.lower()
         positive_title = any(string in title for string in keywords)
         positive_body = any(string in body for string in keywords)
 
-        if submission.id not in already_done and (positive_title or positive_body)
-            msg = '[Potential apartment lead found here](%s) % submission.short_link
-            pr.send_message('user_to_notify', 'Apartment lead', msg) # Replace with username to send to
+        if submission.id not in already_done and (positive_title or positive_body):
+            msg = '[Potential apartment lead found here](%s)' % submission.short_link
+            pr.send_message('USERNAME', 'Apartment lead', msg) # Replace with username to send to
             already_done.append(submission.id)
 
-    sleep(900)
+    time.sleep(300)
+    print 'Cycle complete'
+
